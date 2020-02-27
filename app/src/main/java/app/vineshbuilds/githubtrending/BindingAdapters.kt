823adea20f6d7755e3c10bd5df8c84c2bf827823
Vinesh Raju <vineshraju@rocketmail.com>
2020-02-27 21:38:41 +0530

    Loads Avatar image

--- app/src/main/java/app/vineshbuilds/githubtrending/BindingAdapters.kt
+++ app/src/main/java/app/vineshbuilds/githubtrending/BindingAdapters.kt
@@ -1,10 +1,10 @@
 package app.vineshbuilds.githubtrending
 
 import android.widget.ImageView
-import androidx.annotation.ColorInt
-import androidx.core.graphics.drawable.DrawableCompat
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
-@BindingAdapter("tint")
-fun setTint(view: ImageView, @ColorInt tint: Int?) =
-    tint?.let { DrawableCompat.setTint(view.drawable, tint) }
+@BindingAdapter("imageUrl")
+fun loadImage(view: ImageView, url: String) {
+    Glide.with(view.context).load(url).into(view)
+}
